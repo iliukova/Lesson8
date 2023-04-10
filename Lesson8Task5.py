be09f01e0ff248f83c86974969181b8a9d9ea9b0
Inna <innailiukova@gmail.com>
2023-04-10 20:52:57 +0300

    коментар

--- Lesson8Task5.py
+++ Lesson8Task5.py
@@ -4,3 +4,14 @@
 # > 123,34
 # > one hundred twenty three dollars thirty four cents
 
+import num2word
+def num_in_words(price_num):
+    dollar, cent = price_num.split('.')
+    result = f'{num2word.word(dollar).lower()} dollars ' \
+             f'{num2word.word(cent).lower()} cents'
+    return result
+
+
+price = str(input('price = ').replace(',', '.'))
+
+print(num_in_words(price))
